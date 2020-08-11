25334a0fa259e2b967e15dbd244987efc07b5872
David Bunk <bunk@biologie.uni-muenchen.de>
2020-08-11 10:26:30 +0200

    Added MaskPredictor to API

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from biodetectron.eval import BboxPredictor
+from biodetectron.eval import BboxPredictor, MaskPredictor
 
 import bentoml
 from bentoml.adapters import ImageInput
 from bentoml.artifact import PytorchModelArtifact
 
-predictor = BboxPredictor('/home/bunk/MitoScanner/boxes.yaml', '/home/bunk/MitoScanner/boxes_final.pth')
+box_predictor = BboxPredictor('/home/bunk/MitoScanner/boxes.yaml', '/home/bunk/MitoScanner/boxes_final.pth')
+mask_predictor = MaskPredictor('/home/bunk/MitoScanner/masks.yaml', '/home/bunk/MitoScanner/masks_final.pth')
 
 @bentoml.env(docker_base_image='davidbunk/biodetectron:latest')
-@bentoml.artifacts([PytorchModelArtifact('mitoscanner')])
+@bentoml.artifacts([PytorchModelArtifact('mitoscanner_box'), PytorchModelArtifact('mitoscanner_mask')])
 class MitoScanner(bentoml.BentoService):
     @bentoml.api(input=ImageInput())
-    def predict(self, imgs):
-        outputs = predictor.detect_one_image(imgs)
+    def predict_box(self, imgs):
+        outputs = box_predictor.detect_one_image(imgs)
+        return outputs
+
+    @bentoml.api(input=ImageInput())
+    def predict_mask(self, imgs):
+        outputs = mask_predictor.detect_one_image(imgs)
         return outputs
 
 svc = MitoScanner()
-svc.pack('mitoscanner', predictor.model)
+svc.pack('mitoscanner_box', box_predictor.model)
+svc.pack('mitoscanner_mask', mask_predictor.model)
 
 saved_path = svc.save()
